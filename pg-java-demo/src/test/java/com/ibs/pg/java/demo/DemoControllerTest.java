f20f16d61053b8ac11d1859b91fce8da3510fc9a
guocehua <guocehua@payingcloud.cn>
2017-09-22 10:07:28 +0800

    修改页面3

---
+++ pg-java-demo/src/test/java/com/ibs/pg/java/demo/DemoControllerTest.java
@@ -0,0 +1,13 @@
+package com.ibs.pg.java.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DemoControllerTest {
+    @Test
+    public void placeOrder() throws Exception {
+
+    }
+
+}
