6d0b5c3a45a53867e19fe9d7977c7e65f3382078
黄玉强 <hyq@payingcloud.cn>
2017-09-27 16:46:54 +0800

    clean unused

--- pg-java/src/main/java/com/ibs/pg/java/model/CitizenIdType.java
+++ pg-java/src/main/java/com/ibs/pg/java/model/CitizenIdType.java
@@ -1,15 +1,5 @@
 package com.ibs.pg.java.model;
 
 public enum CitizenIdType {
-    IDENTITY_CARD("1");
-
-    private final String code;
-
-    CitizenIdType(String code) {
-        this.code = code;
-    }
-
-    public String getCode() {
-        return code;
-    }
+    IDENTITY_CARD
 }
--- pg-java/src/main/java/com/ibs/pg/java/model/ItemType.java
+++ pg-java/src/main/java/com/ibs/pg/java/model/ItemType.java
@@ -4,18 +4,8 @@ package com.ibs.pg.java.model;
  * 交易明细-商品类型枚举类
  */
 public enum ItemType {
-    CLOTHING(1),        //1-服装
-    FOOD(2),        //2-食品
-    ELECTRONIC(3),        //3-电子产品
-    OTHER(4);        //4-其他
-
-    private int itemType;
-
-    private ItemType(int value) {
-        this.itemType = value;
-    }
-
-    public int getItemTypeToInt() {
-        return itemType;
-    }
+    CLOTHING,    // 服装
+    FOOD,        // 食品
+    ELECTRONIC,  // 电子产品
+    OTHER        // 其他
 }
